7f1d8afb106dd0ba34aefaacef4d82cbb2d7060b
ryanjnam99 <ryanjnam@yahoo.com>
2023-05-24 07:56:18 -0700

    updated-2

---
+++ script.js
@@ -0,0 +1,28 @@
+var backgroundinfo = document.querySelector(".about");
+
+function press() {
+    backgroundinfo.scrollIntoView(true);
+}
+
+var myproject = document.querySelector(".project");
+function press2() {
+    myproject.scrollIntoView(true);
+}
+
+var myskills = document.querySelector(".skill");
+function press3() {
+    myskills.scrollIntoView(true);
+}
+
+function press5() {
+    window.open("https://mail.yahoo.com/d/folders/1");
+}
+
+function navigation2() {
+    window.open("https://github.com/ryanjnam99");
+}
+
+function press4() {
+    window.open("https://github.com/ryanjnam99/DojoWeather");
+}
+
